82aab9691f6e467ea6b3e332f0bede4657db992c
Chiggy-Playz <garg.chirag2004@gmail.com>
2025-02-11 20:53:42 +0530

    Change default port

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     ntfy_topic: str = ""
-    port: int = 6789
+    port: int = 8000
     sqlite_url: str = "sqlite:///tasks.db"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
